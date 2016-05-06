75a7282a9cdf244c31341e515000f69bfb79e514
odeheurles <mail@odeheurles.com>
2016-05-06 16:35:07 +0100

    CollectionUtil + tests

---
+++ src/Adaptive.Agrona.Tests/Collections/CollectionUtilTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Adaptive.Agrona.Collections;
+using NUnit.Framework;
+
+namespace Adaptive.Agrona.Tests.Collections
+{
+    [TestFixture]
+    public class CollectionUtilTest
+    {
+        [Test]
+        public void GetOrDefaultUsesSupplier()
+        {
+            var ints = new Dictionary<int?, int?>();
+            var result = CollectionUtil.GetOrDefault(ints, 0, x => x + 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetOrDefaultDoesNotCreateNewValueWhenOneExists()
+        {
+            var ints = new Dictionary<int?, int?> {[0] = 0};
+            var result = CollectionUtil.GetOrDefault(ints, 0, (x) =>
+            {
+                Assert.Fail("Shouldn't be called");
+                return x + 1;
+            });
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
