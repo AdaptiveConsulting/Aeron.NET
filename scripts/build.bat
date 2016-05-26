67c3a0cf6be84faee3aac74b9b1504b0444b9e82
Lee Campbell <lee.ryan.campbell@gmail.com>
2016-05-26 13:32:08 +0800

    Allow build script to be run from other dir

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,3 +1,5 @@
 @echo off
-nuget restore ..\src\Adaptive.Aeron.sln
-call "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" /p:Configuration=Release /p:Platform="x64" ..\src\Adaptive.Aeron.sln
+pushd %~dp0..
+nuget restore .\src\Adaptive.Aeron.sln
+call "C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" /p:Configuration=Release /p:Platform="x64" .\src\Adaptive.Aeron.sln
+popd
