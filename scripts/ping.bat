6ada8f9386b0ee3681b23b1c835edf6e64aa73aa
James Watson <james@weareadaptive.com>
2016-06-13 14:26:24 +0100

    Increase number of messages for ping scripts. (5881753)

--- scripts/ping.bat
+++ scripts/ping.bat
@@ -1,4 +1,4 @@
 @echo off
 call ..\src\Samples\Adaptive.Aeron.Samples.Ping\bin\x64\Release\Adaptive.Aeron.Samples.Ping.exe ^
-	-Daeron.sample.messages=100000 ^
+	-Daeron.sample.messages=1000000 ^
 	-Daeron.sample.messageLength=32
