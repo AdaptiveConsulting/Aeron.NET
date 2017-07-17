57d89cbefc50fc38d9a3777d0f34542c3dd058f2
James Watson <james@weareadaptive.com>
2017-07-17 18:36:18 +0100

    Build Driver Nuget

--- scripts/build-nuget-packages.bat
+++ scripts/build-nuget-packages.bat
@@ -2,4 +2,5 @@
 pushd %~dp0..
 call .\scripts\build.bat
 call .\scripts\nuget pack .\src\Adaptive.Aeron\Adaptive.Aeron.csproj -IncludeReferencedProjects -Prop Configuration=Release
+call .\scripts\nuget pack .\driver\Aeron.Driver.nuspec
 popd
