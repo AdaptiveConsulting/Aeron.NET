5bbc5e6143aa7eb5da3047c9bb96bb2ee11703ba
James Watson <james@weareadaptive.com>
2017-08-15 13:36:33 +0100

    default driver less CPU instensive

--- driver/start-media-driver.bat
+++ driver/start-media-driver.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Media Driver...
 java -cp media-driver.jar ^
-	io.aeron.driver.MediaDriver ipc.properties 
+	io.aeron.driver.MediaDriver -Daeron.threading.mode=SHARED
 echo Media Driver Stopped.
 pause
