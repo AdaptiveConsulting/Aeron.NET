cea6db357f52655439af16b4f236af62cf50243e
James Watson <james@weareadaptive.com>
2017-08-17 12:49:36 +0100

    Release 1.3.1

--- src/Adaptive.Aeron/Properties/AssemblyInfo.cs
+++ src/Adaptive.Aeron/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Adaptive.Aeron")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,8 +48,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 
 [assembly: InternalsVisibleTo("Adaptive.Aeron.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
