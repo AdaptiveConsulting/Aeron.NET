18da561327d305ff87f9ef47e0aa07a8af4a8042
James Watson <james@weareadaptive.com>
2017-08-17 12:46:32 +0100

    add NoOpLock

---
+++ src/Adaptive.Agrona/Concurrent/NoOpLock.cs
@@ -0,0 +1,18 @@
+﻿namespace Adaptive.Agrona.Concurrent
+{
+    public class NoOpLock : ILock
+    {
+        public void Lock()
+        {
+        }
+
+        public void Unlock()
+        {
+        }
+
+        public bool TryLock()
+        {
+            return true;
+        }
+    }
+}
