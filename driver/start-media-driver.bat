43b79268dd2ad1b49a687b895057b69f49711618
James Watson <james@weareadaptive.com>
2018-01-30 09:30:21 +0000

    Update MediaDriver

--- driver/start-media-driver.bat
+++ driver/start-media-driver.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Media Driver Started...
 java -cp media-driver.jar ^
-	io.aeron.driver.MediaDriver ipc.properties
+	io.aeron.cluster.ClusteredMediaDriver
 echo Media Driver Stopped.
 pause
--- scripts/build-nuget-packages.bat
+++ scripts/build-nuget-packages.bat
@@ -1,7 +1,9 @@
 @echo off
 pushd %~dp0..
 call .\scripts\build.bat
-call dotnet pack .\src\Adaptive.Aeron\Adaptive.Aeron.csproj /p:Configuration=Release
-call dotnet pack .\src\Adaptive.Agrona\Adaptive.Agrona.csproj /p:Configuration=Release
-call .\scripts\nuget pack .\driver\Aeron.Driver.nuspec
+call dotnet pack .\src\Adaptive.Aeron\Adaptive.Aeron.csproj -c Release -o ..\..\nupkgs
+call dotnet pack .\src\Adaptive.Agrona\Adaptive.Agrona.csproj -c Release -o ..\..\nupkgs 
+call dotnet pack .\src\Adaptive.Cluster\Adaptive.Cluster.csproj -c Release -o ..\..\nupkgs
+call dotnet pack .\src\Adaptive.Archiver\Adaptive.Archiver.csproj -c Release -o ..\..\nupkgs
+call .\scripts\nuget pack .\driver\Aeron.Driver.nuspec ..\nupkgs
 popd
