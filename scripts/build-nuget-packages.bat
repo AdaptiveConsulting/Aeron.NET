53c85bb550facd9e305bd38e58d71a73e9743f8a
James Watson <james@weareadaptive.com>
2018-03-13 13:43:45 +0000

    Tidy up build-nuget-packages.bat

--- scripts/build-nuget-packages.bat
+++ scripts/build-nuget-packages.bat
@@ -1,9 +1,8 @@
 @echo off
 pushd %~dp0..
-call .\scripts\build.bat
-call dotnet pack .\src\Adaptive.Aeron\Adaptive.Aeron.csproj -c Release -o ..\..\nupkgs
-call dotnet pack .\src\Adaptive.Agrona\Adaptive.Agrona.csproj -c Release -o ..\..\nupkgs 
-call dotnet pack .\src\Adaptive.Cluster\Adaptive.Cluster.csproj -c Release -o ..\..\nupkgs
-call dotnet pack .\src\Adaptive.Archiver\Adaptive.Archiver.csproj -c Release -o ..\..\nupkgs
-call .\scripts\nuget pack .\driver\Aeron.Driver.nuspec ..\nupkgs
+call dotnet pack src\Adaptive.Aeron\Adaptive.Aeron.csproj                -c Release --output ..\..\nupkgs
+call dotnet pack src\Adaptive.Agrona\Adaptive.Agrona.csproj              -c Release --output ..\..\nupkgs 
+call dotnet pack src\Adaptive.Cluster\Adaptive.Cluster.csproj            -c Release --output ..\..\nupkgs
+call dotnet pack src\Adaptive.Archiver\Adaptive.Archiver.csproj          -c Release --output ..\..\nupkgs
+call .\scripts\nuget pack .\driver\Aeron.Driver.nuspec                   -OutputDirectory nupkgs
 popd
