4ec7f47c3caa0da2c3f39256df8351230cc54b1b
James Watson <james@weareadaptive.com>
2018-03-27 11:12:25 +0100

    Adjust BackoffIdleStrategy defaults

--- src/Adaptive.Agrona/Concurrent/Configuration.cs
+++ src/Adaptive.Agrona/Concurrent/Configuration.cs
@@ -10,7 +10,7 @@
         /// <summary>
         /// Yield the thread so others can run before backing off to parking.
         /// </summary>
-        public const long IDLE_MAX_YIELDS = 20;
+        public const long IDLE_MAX_YIELDS = 40;
 
         /// <summary>
         /// Park for the minimum period of time which is typically 50-55 microseconds on 64-bit non-virtualised Linux.
@@ -22,7 +22,6 @@
         /// <summary>
         /// Maximum back-off park time which doubles on each interval stepping up from the min park idle.
         /// </summary>
-        public static readonly long IDLE_MAX_PARK_MS = 1000;
-
+        public static readonly long IDLE_MAX_PARK_MS = 16;
     }
 }
