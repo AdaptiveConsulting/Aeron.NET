682339f6ef89762c8743ba298078ec13323a86be
Victor Baybekov <v.baybekov@abc-arbitrage.com>
2021-07-07 18:41:02 +0200

    Disable native config print

--- src/Adaptive.Aeron.Driver.Native.Tests/DriverContextUtil.cs
+++ src/Adaptive.Aeron.Driver.Native.Tests/DriverContextUtil.cs
@@ -18,7 +18,7 @@ namespace Adaptive.Aeron.Driver.Native.Tests
                 .TermBufferLength(128 * 1024)
                 .DirDeleteOnStart(true)
                 .DirDeleteOnShutdown(true)
-                .PrintConfigurationOnStart(true)
+                .PrintConfigurationOnStart(false)
                 .LoggerInfo(s => Console.WriteLine($"INFO: {s}"))
                 .LoggerWarning(s => Console.WriteLine($"WARN: {s}"))
                 .LoggerError(s => Console.Error.WriteLine($"ERROR: {s}"));
