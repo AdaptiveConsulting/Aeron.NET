308079e44f2c57b47891184bcfb4f6555cca0fa4
James Watson <james@weareadaptive.com>
2022-01-25 00:42:38 +0000

    Fix build scripts.

--- RELEASE.md
+++ RELEASE.md
@@ -1,4 +1,4 @@
 #### Port
 Aeron.NET has been ported against Java version:
-- Agrona: 1.0.7
-- Aeron:  1.28.2
+- Agrona: 1.14.0
+- Aeron:  1.37.0
--- scripts/build-nuget-packages.bat
+++ scripts/build-nuget-packages.bat
@@ -1,8 +1,8 @@
 @echo off
 pushd %~dp0..
-call dotnet pack src\Adaptive.Aeron\Adaptive.Aeron.csproj                -c Release --output ..\..\nupkgs
-call dotnet pack src\Adaptive.Agrona\Adaptive.Agrona.csproj              -c Release --output ..\..\nupkgs 
-call dotnet pack src\Adaptive.Cluster\Adaptive.Cluster.csproj            -c Release --output ..\..\nupkgs
-call dotnet pack src\Adaptive.Archiver\Adaptive.Archiver.csproj          -c Release --output ..\..\nupkgs
+call dotnet pack src\Adaptive.Aeron\Adaptive.Aeron.csproj                -c Release --output nupkgs
+call dotnet pack src\Adaptive.Agrona\Adaptive.Agrona.csproj              -c Release --output nupkgs 
+call dotnet pack src\Adaptive.Cluster\Adaptive.Cluster.csproj            -c Release --output nupkgs
+call dotnet pack src\Adaptive.Archiver\Adaptive.Archiver.csproj          -c Release --output nupkgs
 call .\scripts\nuget pack .\driver\Aeron.Driver.nuspec                   -OutputDirectory nupkgs
 popd
