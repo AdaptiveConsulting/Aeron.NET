3f9f25f220602a6733243acadaa2cfc4dd0da000
rafalpiotrowski <rafalpiotrowski@users.noreply.github.com>
2023-03-12 13:42:54 +0100

    script to run aeron stat on linux

---
+++ driver/aeron-stat.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------
+# startup script.
+# ---------------------------------------------------------------------
+
+message()
+{
+  TITLE="Cannot start AeronStat"
+  if [ -n "$(command -v zenity)" ]; then
+    zenity --error --title="$TITLE" --text="$1" --no-wrap
+  elif [ -n "$(command -v kdialog)" ]; then
+    kdialog --error "$1" --title "$TITLE"
+  elif [ -n "$(command -v notify-send)" ]; then
+    notify-send "ERROR: $TITLE" "$1"
+  elif [ -n "$(command -v xmessage)" ]; then
+    xmessage -center "ERROR: $TITLE: $1"
+  else
+    printf "ERROR: %s\n%s\n" "$TITLE" "$1"
+  fi
+}
+
+if [ -z "$(command -v uname)" ] || [ -z "$(command -v realpath)" ] || [ -z "$(command -v dirname)" ] || [ -z "$(command -v cat)" ] || \
+   [ -z "$(command -v grep)" ]; then
+  TOOLS_MSG="Required tools are missing:"
+  for tool in uname realpath grep dirname cat ; do
+     test -z "$(command -v $tool)" && TOOLS_MSG="$TOOLS_MSG $tool"
+  done
+  message "$TOOLS_MSG (SHELL=$SHELL PATH=$PATH)"
+  exit 1
+fi
+
+# shellcheck disable=SC2034
+GREP_OPTIONS=''
+OS_TYPE=$(uname -s)
+OS_ARCH=$(uname -m)
+
+# ---------------------------------------------------------------------
+# Ensure $IDE_HOME points to the directory where the IDE is installed.
+# ---------------------------------------------------------------------
+AERON_HOME=$(dirname "$(realpath "$0")")
+DRIVER_HOME=$(dirname "${AERON_HOME}/driver")
+CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+
+# ---------------------------------------------------------------------
+# Locate a JRE installation directory command -v will be used to run the IDE.
+# Try (in order): $RIDER_JDK, .../rider.jdk, .../jbr, $JDK_HOME, $JAVA_HOME, "java" in $PATH.
+# ---------------------------------------------------------------------
+JRE=""
+
+# shellcheck disable=SC2153
+if [ -z "$JRE" ]; then
+  if [ -n "$JDK_HOME" ] && [ -x "$JDK_HOME/bin/java" ]; then
+    JRE="$JDK_HOME"
+  elif [ -n "$JAVA_HOME" ] && [ -x "$JAVA_HOME/bin/java" ]; then
+    JRE="$JAVA_HOME"
+  fi
+fi
+
+if [ -z "$JRE" ]; then
+  JAVA_BIN=$(command -v java)
+else
+  JAVA_BIN="$JRE/bin/java"
+fi
+
+if [ -z "$JAVA_BIN" ] || [ ! -x "$JAVA_BIN" ]; then
+  message "No JRE found. Please make sure \$RIDER_JDK, \$JDK_HOME, or \$JAVA_HOME point to valid JRE installation."
+  exit 1
+fi
+
+CLASS_PATH="$DRIVER_HOME/media-driver.jar"
+
+# ---------------------------------------------------------------------
+# Run the AeronStat.
+# ---------------------------------------------------------------------
+IFS="$(printf '\n\t')"
+# shellcheck disable=SC2086
+exec "$JAVA_BIN" \
+    -classpath "$CLASS_PATH" -Daeron.dir=/dev/shm/aeron-"$USER" io.aeron.samples.AeronStat
