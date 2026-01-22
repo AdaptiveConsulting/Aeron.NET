714ac992f69578aee3d8bd0341cc0c42e9ce45db
Nadia Aina <113453463+Nadia-Adaptive@users.noreply.github.com>
2026-01-22 13:51:16 +0000

    set aeron version to 1.49.0

--- src/Adaptive.Aeron/AeronVersion.cs
+++ src/Adaptive.Aeron/AeronVersion.cs
@@ -2,9 +2,9 @@
 {
     public class AeronVersion
     {
-        public const string VERSION = "1.43.0";
+        public const string VERSION = "1.49.0";
         public const int MAJOR_VERSION = 1;
-        public const int MINOR_VERSION = 43;
+        public const int MINOR_VERSION = 49;
         public const int PATCH_VERSION = 0;
     }
 }
